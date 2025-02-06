21534691045edcdd486571e52ef7456551f899e8
Brian Yang <yangbri1@msu.edu>
2025-02-06 22:16:00 +0000

    Instilled CRUD capabilities to 'StoreService.java' --- used 'GroceryService.java' as reference material

--- src/main/java/Lab/Service/StoreService.java
+++ src/main/java/Lab/Service/StoreService.java
@@ -20,14 +20,17 @@ public class StoreService {
      * @return the persisted store
      */
     public Store persistStore(Store store){
-        return null;
+        // create a new DB record for 'store' entity
+        return storeRepository.save(store);
     }
     /**
      * TODO: get all store entities
      * @return all store entities
      */
     public List<Store> getAllStores(){
-        return null;
+        /* call upon .findAll() method to return all 'store' entities/records stored in the DB table
+         as a List container of 'Store' elements */
+        return storeRepository.findAll();
     }
     /**
      * TODO: given an id of a store, return the store.
@@ -36,13 +39,23 @@ public class StoreService {
      * @return a store entity
      */
     public Store getStoreById(long id){
+        // initialize an Optional container obj w/ respective entity retrieved from 'Store' DB table using .findById() method from Spring framework
+        Optional<Store> optionalStore = storeRepository.findById(id);
+        // check to see if freshly made 'optionalStore' Optional container is not null/falsy value via .isPresent() method from 'java.util.Optional' package
+        // aka if the entity tied to given parameter 'id' exists ... 
+        if(optionalStore.isPresent() == true){
+            // call built-in .get() method from 'Optional' package to retrieve the entity/record
+            return optionalStore.get();
+        }
+        // otw if the entity DNE ... return default value 'null'
         return null;
     }
     /**
      * TODO: given an id of an existing store, delete the store
      */
     public void deleteStore(long id){
-//        code here
+        // invoke built-in .deleteById() method from Spring framework's CrudRepository to do so
+        storeRepository.deleteById(id);     // notice: no 'return' statement here as method is 'void' type -- no return!
     }
     /**
      * TODO: given an id and some replacement data for a store, overwrite the data of an existing store,
@@ -50,7 +63,23 @@ public class StoreService {
      * @return the updated store entity
      */
     public Store updateStore(long id, Store replacement){
-        return null;
+        // call .findById() method to retrieve corresponding 'Store' entity by its 'id'
+        // ... and assign retrieved entity as to Optional<Store> container 'optionalStore'
+        Optional<Store> optionalStore = storeRepository.findById(id);
+        // check to see not empty/false
+        if(optionalStore.isPresent()){
+            // attempts to .get() entity from 'optionalStore' container
+            Store store = optionalStore.get();
+            // implement setter method .setName() to replace entity w/ specific store 'id' ...
+            // w/ given 'replace' Store's name
+            store.setName(replacement.getName());
+            // .save() the changes
+            storeRepository.save(store);
+        }
+        // call entity/record w/ this 'id' & return 'Store' results
+        return optionalStore.get(); 
+        // return store;    --- this one would NOT work as 'store' was initialized within if block scope --- Out of Scope
     }
 
 }
+
